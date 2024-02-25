19dc06f41f5c392639503ef3d931470d299600df
ragingpenguin <joshuahuntereflin@gmail.com>
2024-02-24 21:37:34 -0500

    testing that git config editor is not nano(passed) its neovim!

--- factorial.lua
+++ factorial.lua
@@ -1,16 +1,17 @@
 -- defines a factorial
 function fact(n)
-   if n == 0 then
-     return 1
-   else
-     return n * fact(n-1)
+  if n == 0 then
+    return 1
+  else
+    return n * fact(n - 1)
   end
 end
 
 print("enter a number:")
 local a = io.read("*number") -- reads a number
 if a then
-  print( fact(a))
+  print(fact(a))
 else
   print("Wooooops")
+  print("wow")
 end
